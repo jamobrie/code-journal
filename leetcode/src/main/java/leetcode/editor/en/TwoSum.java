db1ab87cf2693e0db4748d2d39c231e6c9c6109c
James O'Brien <jamesobrien442@gmai.com>
2025-03-07 22:23:02 +0000

    Set up of leetcode plugin to generate problems for me :)

---
+++ leetcode/src/main/java/leetcode/editor/en/TwoSum.java
@@ -0,0 +1,18 @@
+package leetcode.editor.en;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    static //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            return nums;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
