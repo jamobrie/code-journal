ae2f3edb02597c6c3ff5811f65cfadedcf11cbf4
James O'Brien <jamesobrien442@gmai.com>
2025-03-08 12:11:11 +0000

    Basic BruteForce Solution to 2 sum problem

--- leetcode/src/main/java/leetcode/editor/en/TwoSum.java
+++ leetcode/src/main/java/leetcode/editor/en/TwoSum.java
@@ -1,14 +1,29 @@
 package leetcode.editor.en;
 
+import java.util.Arrays;
+
 public class TwoSum {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        TwoSum outer = new TwoSum();
+        Solution solution = outer.new Solution();
+        int[] answer = solution.twoSum(new int[]{2, 3, 5, 20, 60, 99, 7, 11, 15}, 65);
+        System.out.println("Solution: " + Arrays.toString(answer));
     }
 
-    static //leetcode submit region begin(Prohibit modification and deletion)
+    //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
+        //Brute force solution - Time complexity: O(n)2 .... Space complexity: O(1)
         public int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] == target - nums[i]) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+
             return nums;
         }
     }
